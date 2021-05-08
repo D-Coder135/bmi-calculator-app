7ce01a49abc5feb5833d84840733f16120a58ded
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 20:03:21 +0530

    add the color property inside the decoration property

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,6 +16,7 @@ class _InputPageState extends State<InputPage> {
         // color: Color(0xFF1D1E33),
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
+          color: Color(0xFF1D1E33),
           borderRadius: BorderRadius.circular(10.0),
         ),
         height: 200.0,
