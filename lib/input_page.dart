6b6e43a1386d53c43843185548b333bf82edab3f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 20:40:48 +0530

    Add a Row() widget as a child of one of the Expanded() widget which is the child of the Column() widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,15 +12,16 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        // color: Color(0xFF1D1E33), Whenever we use decoration property inside the Container() widget then its compulsory that the color property should be specified inside the decoration property.
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        // height: 200.0,
-        // width: 170.0,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[],
+            ),
+          ),
+          Expanded(child: null),
+          Expanded(child: null)
+        ],
       ),
       //Center(
       // child: Text('Body Text'),
