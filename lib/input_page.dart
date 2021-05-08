43757d064fe5d0c551663119a426b6e380900ae5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 22:38:12 +0530

    Comment out the constructor of the ReusableCard class

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -51,9 +51,9 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key key,
-  }) : super(key: key);
+  // const ReusableCard({
+  //   Key key,
+  // }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
