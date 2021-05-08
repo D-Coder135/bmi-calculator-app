80b71c1a0946ffb5a241bd92fd672b301398f2fa
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 19:12:33 +0530

    add the Color() widget with the same hashcode inside the property scaffoldBackgroundColor inside the ThemeData() widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class BMICalculator extends StatelessWidget {
       theme: ThemeData(
         // The hexcode for the opaque is 0xFF. Add this in place of # symbol in every hexcode.
         primaryColor: Color(0xFF0A0D22),
+        scaffoldBackgroundColor: Color(0xFF0A0D22),
       ),
       home: InputPage(),
     );
