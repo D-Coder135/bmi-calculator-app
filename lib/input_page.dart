ede7900d362980eb425713cf9e8129b8c212c4a3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 20:46:58 +0530

    Copy the code from the first Expanded() widget to the last Expanded() widget inside the Column() widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -56,7 +56,36 @@ class _InputPageState extends State<InputPage> {
               // width: 170.0,
             ),
           ),
-          Expanded(child: null)
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    // color: Color(0xFF1D1E33), Whenever we use decoration property inside the Container() widget then its compulsory that the color property should be specified inside the decoration property.
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    // height: 200.0,
+                    // width: 170.0,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    // color: Color(0xFF1D1E33), Whenever we use decoration property inside the Container() widget then its compulsory that the color property should be specified inside the decoration property.
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    // height: 200.0,
+                    // width: 170.0,
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
       //Center(
