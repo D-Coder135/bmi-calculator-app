7e5e1a3b41eb2401038e6b8f6b541753d0e111ef
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 19:37:20 +0530

    Copied some part of the old commented out code inside the copyWith() widget inside the MaterialApp() widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,24 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark()
-      // ThemeData(
-      //   // The hashcode for the opaque is 0xFF. Add this in place of # symbol in every hexcode.
-      //   primaryColor: Color(0xFF0A0D22),
-      //   scaffoldBackgroundColor: Color(0xFF0A0D22),
-      //   textTheme: TextTheme(
-      //     body1: TextStyle(
-      //         color: Colors
-      //             .white), // This is the method by which we can customize the Text Style by our own.
-      //   ),
-      // ),
+      theme: ThemeData.dark().copyWith(
+        // The hashcode for the opaque is 0xFF. Add this in place of # symbol in every hexcode.
+        primaryColor: Color(0xFF0A0D22),
+        scaffoldBackgroundColor: Color(
+            0xFF0A0D22), // This is the method by which we can customize the Text Style by our own.
+      ),
       home: InputPage(),
     );
+    // ThemeData(
+    //   // The hashcode for the opaque is 0xFF. Add this in place of # symbol in every hexcode.
+    //   primaryColor: Color(0xFF0A0D22),
+    //   scaffoldBackgroundColor: Color(0xFF0A0D22),
+    //   textTheme: TextTheme(
+    //     body1: TextStyle(
+    //         color: Colors
+    //             .white), // This is the method by which we can customize the Text Style by our own.
+    //   ),
+    // ),
   }
 }
 
