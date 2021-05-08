0f11a9d9dea5cf244d2384b3534f1b48b6cb380d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 20:44:29 +0530

    Add the old code of the Container() widget as the child of one of the Expanded() widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -17,7 +17,18 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: <Widget>[
-                Expanded(child: null),
+                Expanded(
+                  child: Container(
+                    // color: Color(0xFF1D1E33), Whenever we use decoration property inside the Container() widget then its compulsory that the color property should be specified inside the decoration property.
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    // height: 200.0,
+                    // width: 170.0,
+                  ),
+                ),
                 Expanded(child: null),
               ],
             ),
