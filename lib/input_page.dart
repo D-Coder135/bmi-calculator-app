51ca5b22100de917c87a8370f8678555d8f83fee
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 23:18:20 +0530

    add a const field inside the class _InputPageState name bottomContainerColor()

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -10,6 +10,7 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     const bottomContainerHeight = 80.0;
     const activeCardColor = Color(0xFF1D1E33);
+    const bottomContainerColor = Color(0xFFEB1555);
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
