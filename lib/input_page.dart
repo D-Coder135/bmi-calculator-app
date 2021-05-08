b50b8f14643ca9dc4dd54eeec8f23d9531fcafb4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 20:04:57 +0530

    Add some comment to the code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,7 +13,7 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Container(
-        // color: Color(0xFF1D1E33),
+        // color: Color(0xFF1D1E33), Whenever we use decoration property inside the Container() widget then its compulsory that the color property should be specified inside the decoration property.
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           color: Color(0xFF1D1E33),
