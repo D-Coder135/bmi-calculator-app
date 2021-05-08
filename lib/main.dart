39929eb9f0b4fd9cb51d2875ce2d2c24582d39e6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 18:59:57 +0530

    Add a Color() widget along with the hexcode of the color inside the primaryColor property of the ThemeData() widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(),
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0D22);
+      ),
       home: InputPage(),
     );
   }
