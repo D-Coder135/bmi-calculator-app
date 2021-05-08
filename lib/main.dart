e1ce1905682b91fbba11c112e84680985d4ad1e0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 19:32:40 +0530

    Comment out some part of the code

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,17 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        // The hashcode for the opaque is 0xFF. Add this in place of # symbol in every hexcode.
-        primaryColor: Color(0xFF0A0D22),
-        scaffoldBackgroundColor: Color(0xFF0A0D22),
-        textTheme: TextTheme(
-          body1: TextStyle(
-              color: Colors
-                  .white), // This is the method by which we can customize the Text Style by our own.
-        ),
-      ),
+      theme:
+      // ThemeData(
+      //   // The hashcode for the opaque is 0xFF. Add this in place of # symbol in every hexcode.
+      //   primaryColor: Color(0xFF0A0D22),
+      //   scaffoldBackgroundColor: Color(0xFF0A0D22),
+      //   textTheme: TextTheme(
+      //     body1: TextStyle(
+      //         color: Colors
+      //             .white), // This is the method by which we can customize the Text Style by our own.
+      //   ),
+      // ),
       home: InputPage(),
     );
   }
