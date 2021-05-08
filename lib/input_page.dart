5fffa5f482aca2cc4af0616f0910478853ca49d9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-08 19:39:07 +0530

    Copied some part of the code from the main.dart to input_page.dart

---
+++ lib/input_page.dart
@@ -0,0 +1,21 @@
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
