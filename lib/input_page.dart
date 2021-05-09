bf3f2771dce3a3cfbcf5fc6d3a394733113ea313
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 23:33:27 +0530

    initialize the height field with the method call of round() method using the parameter newValue

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -132,7 +132,9 @@ class _InputPageState extends State<InputPage> {
                         activeColor: Color(0xFFEB1555),
                         inactiveColor: Color(0xFF8D8E98),
                         onChanged: (double newValue) {
-                          setState(() {});
+                          setState(() {
+                            height = newValue.round();
+                          });
                         },
                       ),
                     ],
