1a063c39b9bec33b78a78df68e4bb1856c1ed883
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 13:26:34 +0530

    make the fields global

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,6 +3,11 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -11,10 +16,6 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    const bottomContainerHeight = 80.0;
-    const activeCardColor = Color(0xFF1D1E33);
-    const inactiveCardColor = Color(0xFF111328);
-    const bottomContainerColor = Color(0xFFEB1555);
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
