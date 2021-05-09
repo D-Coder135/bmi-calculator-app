6d10249341347d9d32d80354aafc5afe1df5eeaa
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 22:12:45 +0530

    Initialize the field inside the constructor

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -9,7 +9,7 @@ class ReusableCard extends StatelessWidget {
   final Widget cardChild;
   final Function onPress;
 
-  ReusableCard({@required this.colour, this.cardChild});
+  ReusableCard({@required this.colour, this.cardChild, this.onPress});
 
   @override
   Widget build(BuildContext context) {
