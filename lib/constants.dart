50a4fc607fe79be6788155387e77767886e7d018
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 22:32:56 +0530

    refactor name of a constant field inside file constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 const kBottomContainerHeight = 80.0;
 const kActiveCardColor = Color(0xFF1D1E33);
 const kInactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
+const kBottomContainerColor = Color(0xFFEB1555);
 
 const label = TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
