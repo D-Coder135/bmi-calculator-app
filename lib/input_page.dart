92e755491bdf965af61a36c9b065afd4c0edc9e6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 22:38:46 +0530

    import constants.dart file inside the input_page.dart file

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
+import 'constants.dart';
 
 enum Gender {
   MALE,
