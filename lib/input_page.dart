b182a2784e52a992c03a6717977d0a3d5cb2c5fc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 22:29:37 +0530

    Cut some part of the code from input_page.dart file

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,11 +3,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
-
 enum Gender {
   MALE,
   FEMALE,
