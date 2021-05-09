e995514ff224c7541b284c570d40ac92bc9f81e8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 11:25:51 +0530

    remove the copied content from the input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -68,33 +68,6 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class IconContent extends StatelessWidget {
-  final IconData iconContent;
-  final String textContent;
-
-  IconContent(this.iconContent, this.textContent);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-          iconContent,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          textContent,
-          style: TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0),
-        ),
-      ],
-    );
-  }
-}
-
 class ReusableCard extends StatelessWidget {
   // const ReusableCard({
   //   Key key,
