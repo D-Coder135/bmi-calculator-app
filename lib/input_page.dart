fe21bcecbb3075ba0ad4531ad86977660ee882bc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 23:04:11 +0530

    add one more Text() widget as a child of the Row() widget and pass a text to it

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -114,6 +114,7 @@ class _InputPageState extends State<InputPage> {
                         '180',
                         style: kNumberTextStyle,
                       ),
+                      Text('cm'),
                     ],
                   ),
                 ],
