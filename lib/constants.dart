1261e7d365df9986039a4f2ee480e603df088a0d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 22:54:38 +0530

    add a const field inside the file constants.dart with value equals TextStyle() widget

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,3 +8,7 @@ const kInactiveCardColor = Color(0xFF111328);
 const kBottomContainerColor = Color(0xFFEB1555);
 
 const kLabelTextStyle = TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
