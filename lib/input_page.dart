a8faf84430c37bbd84225275292c6301e41a278e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 23:56:09 +0530

    set the activeTrackColor property for the SliderTheme() widget to white color

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -129,6 +129,7 @@ class _InputPageState extends State<InputPage> {
                   ),
                   SliderTheme(
                     data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
                       thumbColor: Color(0xFFEB1555),
                       thumbShape:
                           RoundSliderThumbShape(enabledThumbRadius: 15.0),
