e7b750d81a557b22ffa68f07b771a338bc2b6134
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 22:30:15 +0530

    paste some part of the code cut from file input_page.dart into file constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+const bottomContainerColor = Color(0xFFEB1555);
+
 const label = TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
