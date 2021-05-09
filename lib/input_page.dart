17cdbd443f2e544fa50e7e6156b2a0967b7abdbb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 13:24:48 +0530

    Initialize a const field inside the build() method of the _InputPageState class

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,6 +13,7 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     const bottomContainerHeight = 80.0;
     const activeCardColor = Color(0xFF1D1E33);
+    const inactiveCardColor = Color(0xFF111328);
     const bottomContainerColor = Color(0xFFEB1555);
     return Scaffold(
       appBar: AppBar(
