3f76403cc4f8f997d2bb54321945b38153738a62
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 11:28:19 +0530

    import icon_content.dart inside input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
 
 class InputPage extends StatefulWidget {
   @override
