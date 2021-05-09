c32df6042195b46ac890b2697f5f0619f73267f2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 11:25:23 +0530

    Copy and paste some part of the code from input_page.dart to icon_content.dart

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,1 +1,28 @@
 import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  final IconData iconContent;
+  final String textContent;
+
+  IconContent(this.iconContent, this.textContent);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconContent,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          textContent,
+          style: TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0),
+        ),
+      ],
+    );
+  }
+}
