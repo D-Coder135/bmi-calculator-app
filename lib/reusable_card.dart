cf67fe3120e001c3b418fa4c467bbd65cda4beec
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-09 11:27:08 +0530

    Copy and paste some part of the code from input_page.dart to reusable_card.dart

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,1 +1,28 @@
 import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  // const ReusableCard({
+  //   Key key,
+  // }) : super(key: key);
+
+  final Color colour;
+  final Widget cardChild;
+
+  ReusableCard({@required this.colour, this.cardChild});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      // color: Color(0xFF1D1E33), Whenever we use decoration property inside the Container() widget then its compulsory that the color property should be specified inside the decoration property.
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        // color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      // height: 200.0,
+      // width: 170.0,
+    );
+  }
+}
