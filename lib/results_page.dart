d0eb8d1ea63b586191b8fa9d0242363e1bb3179b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-11 00:13:45 +0530

    add a AppBar() widget inside the appBar property of the Scaffold() widget

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -5,6 +5,8 @@ class ResultsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
