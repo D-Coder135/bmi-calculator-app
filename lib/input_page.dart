7be2f5ec3072f041804eb8b2b8e86dd5c63f3113
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-10 14:03:17 +0530

    add a SizedBox() widget as a child of the Row() widget in between both the FloatingActionButton() widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -184,6 +184,7 @@ class _InputPageState extends State<InputPage> {
                                     color: Colors.white,
                                   ),
                                 ),
+                                SizedBox(),
                                 FloatingActionButton(
                                   backgroundColor: Color(0xFF4C4F5E),
                                   child: Icon(
