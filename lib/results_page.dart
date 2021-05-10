011b126c6d93cf18e59061d0ae40f81708ee4e86
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-11 00:14:20 +0530

    add a Text() widget inside the title property of the AppBar() widget

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -6,7 +6,9 @@ class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
     );
   }
 }
