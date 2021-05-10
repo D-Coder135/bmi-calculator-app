ac5ce02aa4ef6df4ae8041d714b0fb42c8f521f9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-10 13:53:23 +0530

    Add one more Text() widget as a child of the Column() widget and call the toString() method using the weight field inside it

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -169,6 +169,9 @@ class _InputPageState extends State<InputPage> {
                           'WEIGHT',
                           style: kLabelTextStyle,
                         ),
+                        Text(
+                          weight.toString(),
+                        ),
                       ],
                     ),
                   ),
