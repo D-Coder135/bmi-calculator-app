b341a492cb8f5146561cfd8fb0d2453e11b18e65
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-10 14:50:05 +0530

    add a Column() widget as a cardChild of the ReusableCard() widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -218,6 +218,7 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: kActiveCardColor,
+                    cardChild: Column(),
                   ),
                 ),
               ],
