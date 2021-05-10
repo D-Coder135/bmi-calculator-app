e4698eb4617d95a700a7f831d9cd65ec1fa59915
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-10 15:02:57 +0530

    add a setState() function inside the onPressed function of RoundIconButton() widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -244,7 +244,9 @@ class _InputPageState extends State<InputPage> {
                             ),
                             RoundIconButton(
                               icon: FontAwesomeIcons.plus,
-                              onPressed: () {},
+                              onPressed: () {
+                                setState(() {});
+                              },
                             ),
                           ],
                         ),
