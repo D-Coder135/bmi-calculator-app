b8968a1480cf836425c0bd4053af98d7360fa283
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-12 00:04:53 +0530

    add some comment to the code

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -10,6 +10,7 @@ class CalculatorBrain {
 
   String calculateBMI() {
     _bmi = weight / pow(height / 100, 2);
-    return _bmi.toStringAsFixed(1);
+    return _bmi.toStringAsFixed(
+        1); // This method is used to return the value as a string after fixing the number of significant digits provided in the parameter.
   }
 }
