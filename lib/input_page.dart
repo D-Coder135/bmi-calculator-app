4c0c23aae5eee0a1c340237977632f85d510fba6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-11 20:51:47 +0530

    import the file bottom_button.dart inside the file input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,6 +2,7 @@ import 'package:bmi_calculator/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'bottom_button.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 import 'constants.dart';
