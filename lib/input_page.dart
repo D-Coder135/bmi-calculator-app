f9f02171d9392cd35bdffbfb2e96379b254fc77b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-11 20:53:48 +0530

    Remove the copied code from the file input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -276,25 +276,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-class RoundIconButton extends StatelessWidget {
-  final IconData icon;
-  final Function onPressed;
-
-  RoundIconButton({@required this.icon, @required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      elevation: 6.0,
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-    );
-  }
-}
