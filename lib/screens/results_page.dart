942832b453ca1faa03251ba09c533b1141bae275
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-12 00:30:58 +0530

    remove the default constructor from the class ResultPage

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class ResultsPage extends StatelessWidget {
-  const ResultsPage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
