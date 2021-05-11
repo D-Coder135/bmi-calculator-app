eeb2ae3c2a33cfc5853a33c373e30ee34bccec20
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-11 19:47:32 +0530

    set children property as <Widget> for the Column() widget

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -11,6 +11,7 @@ class ResultsPage extends StatelessWidget {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[],
       ),
     );
   }
