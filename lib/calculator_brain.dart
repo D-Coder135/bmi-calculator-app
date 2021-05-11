1507783170244e23626944f444d7a4d4c92d974a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-12 00:16:29 +0530

    add a return statement inside the body of the else block

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -29,6 +29,8 @@ class CalculatorBrain {
       return 'You have a higher than normal body weight. Try to exercise more.';
     } else if (_bmi >= 18.5) {
       return 'You have a normal body weight. Keep following your current diet and routine';
-    } else {}
+    } else {
+      return 'You have a lower than a normal body weight. You need to add some more stuffs to your diet';
+    }
   }
 }
